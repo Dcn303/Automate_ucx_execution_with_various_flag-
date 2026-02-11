c06a2f9fff400a7602e8d87db02a8617281a8250
Disinlung Kamei <112604796+Dcn303@users.noreply.github.com>
2026-02-11 11:30:26 +0530

    Update generate_many_ucx_flags.sh

--- generate_many_ucx_flags.sh
+++ generate_many_ucx_flags.sh
@@ -21,24 +21,24 @@ cat<<EOF > ${array_ucx_flag[${i}]}.sh
 #! /bin/bash
 #SBATCH -J osu_bw
 #SBATCH --gres=gpu:1
-#SBATCH --partition=intel-hopper
-#SBATCH --account=cdac
+#SBATCH --partition=
+#SBATCH --account=
 #SBATCH --ntasks-per-node=1
 #SBATCH -N 2
 #SBATCH --exclusive
 
 echo \$SLURM_SUBMIT_HOST > log.host_list 2>&1
 ## cuda_12.4 installed locally at home directory
-export PATH=/home/janakiraman/CUDA_RHEL_collections/cuda_12.4/bin:\$PATH
-export LD_LIBRARY_PATH=/home/janakiraman/CUDA_RHEL_collections/cuda_12.4/lib64:\$LD_LIBRARY_PATH
+export PATH=/home/CUDA_RHEL_collections/cuda_12.4/bin:\$PATH
+export LD_LIBRARY_PATH=/home/CUDA_RHEL_collections/cuda_12.4/lib64:\$LD_LIBRARY_PATH
 
 ##location of the UCX_1.17.x with support of cuda 12.4 and gdrcopy
-export PATH=/home/janakiraman/UCX_collections/UCX_1.17.x_with_home_cuda_12.4_support/ucx-1.17.x/build/bin:\$PATH
-export LD_LIBRARY_PATH=/home/janakiraman/UCX_collections/UCX_1.17.x_with_home_cuda_12.4_support/ucx-1.17.x/build/lib:\$LD_LIBRARY_PATH
+export PATH=/home/UCX_collections/UCX_1.17.x_with_home_cuda_12.4_support/ucx-1.17.x/build/bin:\$PATH
+export LD_LIBRARY_PATH=/home/UCX_collections/UCX_1.17.x_with_home_cuda_12.4_support/ucx-1.17.x/build/lib:\$LD_LIBRARY_PATH
 
 ##location of the OpenMPI-5.0.8 with UCX 1.17 and gdrcopy
-export PATH=/home/janakiraman/OpenMPI_5.0.8_Collections/OpenMPI_with_ucx_and_gdrcopy/openmpi-5.0.8/build/bin:\$PATH
-export LD_LIBRARY_PATH=/home/janakiraman/OpenMPI_5.0.8_Collections/OpenMPI_with_ucx_and_gdrcopy/openmpi-5.0.8/build/lib:\$LD_LIBRARY_PATH
+export PATH=/home/OpenMPI_5.0.8_Collections/OpenMPI_with_ucx_and_gdrcopy/openmpi-5.0.8/build/bin:\$PATH
+export LD_LIBRARY_PATH=/home/OpenMPI_5.0.8_Collections/OpenMPI_with_ucx_and_gdrcopy/openmpi-5.0.8/build/lib:\$LD_LIBRARY_PATH
     mpirun -np 2 \\ 
     --bind-to core \\
     --mca pml ucx --mca osc ucx \\
